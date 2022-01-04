f607b49d0ab520b8988118e52b5cd64497bdf1b6
Marcin Drabek <marcindrabek@hotmail.com>
2022-01-04 20:58:10 +0100

    Added Review and Review Module database configuration

---
+++ src/Infrastructure/Database/EntityConfiguration/ReviewModuleEntityConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Core.Models.Reviews;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Database.EntityConfiguration;
+
+public class ReviewModuleEntityConfiguration : IEntityTypeConfiguration<ReviewModule>
+{
+    public void Configure(EntityTypeBuilder<ReviewModule> builder)
+    {
+        builder.ToTable("ReviewModules");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired();
+
+        builder.Property(x => x.Value)
+            .IsRequired();
+
+        builder.Property(x => x.Type)
+            .IsRequired();
+    }
+}
