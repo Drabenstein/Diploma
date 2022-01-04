65d6fdd17f7d1a66d1cd71c373268d6bd2cafed2
Marcin Drabek <marcindrabek@hotmail.com>
2022-01-04 20:58:10 +0100

    Added Review and Review Module database configuration

---
+++ src/Infrastructure/Database/DiplomaDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Core.Models.Reviews;
+using Core.Models.Theses;
+using Core.Models.Topics;
+using Core.Models.Users;
+using Infrastructure.Database.EntityConfiguration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database;
+
+public class DiplomaDbContext : DbContext
+{
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<ReviewModule> ReviewModules { get; set; }
+    
+    public DbSet<Thesis> Theses { get; set; }
+    public DbSet<Declaration> Declarations { get; set; }
+
+    public DbSet<Topic> Topics { get; set; }
+    public DbSet<Application> Applications { get; set; }
+    public DbSet<FieldOfStudy> FieldsOfStudies { get; set; }
+    
+    public DbSet<User> Users { get; set; }
+    public DbSet<Tutor> Tutors { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<AreaOfInterest> AreasOfInterests { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<StudentFieldOfStudy> StudentFieldsOfStudies { get; set; }
+
+    public DiplomaDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DiplomaDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ src/Infrastructure/Database/EntityConfiguration/ReviewEntityConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Core.Models.Reviews;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Database.EntityConfiguration;
+
+public class ReviewEntityConfiguration : IEntityTypeConfiguration<Review>
+{
+    public void Configure(EntityTypeBuilder<Review> builder)
+    {
+        builder.ToTable("Reviews");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Grade)
+            .HasConversion<string?>();
+
+        builder.Property(x => x.IsPublished)
+            .IsRequired();
+
+        builder.Property(x => x.PublishTimestamp);
+        
+        builder.HasMany(x => x.ReviewModules)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Navigation(x => x.ReviewModules)
+            .UsePropertyAccessMode(PropertyAccessMode.Field);
+    }
+}
