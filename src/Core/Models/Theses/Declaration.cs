22e06998f6acd6b323430c1a6c6985427e008f05
Marcin Drabek <marcindrabek@hotmail.com>
2022-01-05 23:11:59 +0100

    WIP

--- src/Core/Models/Theses/Declaration.cs
+++ src/Core/Models/Theses/Declaration.cs
@@ -5,7 +5,7 @@ namespace Core.Models.Theses;
 public record Declaration : EntityBase
 {
     public string ObjectiveOfWork { get; set; }
-    public string OpeartingRange { get; set; }
+    public string OperatingRange { get; set; }
     public string Language { get; set; }
     public DateOnly Date { get; set; }
 }
---
+++ src/Infrastructure/Database/EntityConfiguration/DeclarationEntityConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Core.Models.Theses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Database.EntityConfiguration;
+
+public class DeclarationEntityConfiguration : IEntityTypeConfiguration<Declaration>
+{
+    public void Configure(EntityTypeBuilder<Declaration> builder)
+    {
+        builder.ToTable("Declarations");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Date)
+            .IsRequired();
+
+        builder.Property(x => x.ObjectiveOfWork)
+            .HasMaxLength(4000)
+            .IsRequired();
+
+        builder.Property(x => x.OperatingRange)
+            .HasMaxLength(4000)
+            .IsRequired();
+
+        builder.Property(x => x.Language)
+            .HasMaxLength(255)
+            .IsRequired();
+    }
+}
