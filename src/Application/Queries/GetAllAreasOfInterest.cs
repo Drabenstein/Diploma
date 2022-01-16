a8ead80309fb72e6238e6cb408b16ae4eb115304
Dominik Bordzio <dbordzio@gmail.com>
2022-01-16 20:33:01 +0100

    Add areas of interest endpoint for reviewer view

---
+++ src/Application/Queries/GetAllAreasOfInterest.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common;
+using Core;
+using Dapper;
+using MediatR;
+
+public static class GetAllAreasOfInterest
+{
+    public record Query() : IRequest<IEnumerable<AreaOfInterestDto>>;
+
+    public class Handler : IRequestHandler<Query, IEnumerable<AreaOfInterestDto>>
+    {
+        private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+        public Handler(ISqlConnectionFactory sqlConnectionFactory)
+        {
+            _sqlConnectionFactory = sqlConnectionFactory;
+        }
+
+        private const string Sql = "select area_of_interest_id as \"Id\", \"name\" as \"Name\" from area_of_interest";
+
+        public async Task <IEnumerable<AreaOfInterestDto>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            using var connection = await _sqlConnectionFactory.CreateOpenConnectionAsync().ConfigureAwait(false);
+
+            return await connection.QueryAsync<AreaOfInterestDto>(Sql).ConfigureAwait(false);
+        }
+    }
+
+}
--- src/WebApi/Controllers/ReviewersController.cs
+++ src/WebApi/Controllers/ReviewersController.cs
@@ -13,19 +13,46 @@ namespace WebApi.Controllers;
 public class ReviewersController : BaseApiController
 {
     private readonly IMediator _mediator;
+    private const int DefaultMinNoReviews = 0;
+    private const int DefaultMaxNoReviews = 50;
     public ReviewersController(IMediator mediator)
     {
         _mediator = mediator;
     }
 
+    /// <summary>
+    /// Returns requested page reviewers and their interests
+    /// </summary>
+    /// <param name="AreaOfInterestIds">Array with ids of chosen areas of interest</param>
+    /// <param name="minNoReviews">Minimum number of reviews for a reviewer to be selected</param>
+    /// <param name="maxNoReviews">Maximum number of reviews for a reviewer to be selected</param>
+    /// <param name="page">Request result page, default: 1</param>
+    /// <param name="pageSize">Count of items to return at max, default: 10</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>Paged result with reviewers and their areas of interest</returns>
+    /// <response code="200">Returns requested page reviewers and their interests</response>
     [HttpGet]
     [Route("getReviewersWithInterests")]
     [Authorize(Roles = Role.DeansAssistantRole)]
-    public Task<PagedResultDto<ReviewerWithInterestsDto>> GetReviewers([FromQuery] int minNoReviews, 
-        [FromQuery] int maxNoReviews, [FromQuery] long[] AreaOfInterestIds, [FromQuery] int page = DefaultPage,
+    public Task<PagedResultDto<ReviewerWithInterestsDto>> GetReviewers([FromQuery] long[] AreaOfInterestIds, 
+        [FromQuery] int minNoReviews = DefaultMinNoReviews, [FromQuery] int maxNoReviews = DefaultMaxNoReviews, [FromQuery] int page = DefaultPage,
         [FromQuery] int pageSize = DefaultPageSize, CancellationToken cancellationToken = default)
     {
         return _mediator.Send(new GetReviewersWithInterests.Query(minNoReviews, maxNoReviews, AreaOfInterestIds, page, pageSize), cancellationToken);
+    }
+
+    /// <summary>
+    /// Returns a collection of Areas of interest
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns>Collection of employees' Areas of interest</returns>
+    /// <response code="200"></response>
+    [HttpGet]
+    [Route("getInterestsForReviewers")]
+    [Authorize(Roles = Role.DeansAssistantRole)]
+    public Task<IEnumerable<AreaOfInterestDto>> GetAllAreasOfInterest(CancellationToken cancellationToken)
+    {
+        return _mediator.Send(new GetAllAreasOfInterest.Query(), cancellationToken);
     }
 
 }
