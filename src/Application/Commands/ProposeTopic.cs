8f71f08b326e0b5fa7b0b7937c8ad7ba4d64cb97
Dominik Bordzio <dbordzio@gmail.com>
2022-01-17 16:14:08 +0100

    Add application creation

---
+++ src/Application/Commands/ProposeTopic.cs
@@ -0,0 +1,59 @@
+﻿using Core.Models.Topics;
+using Core.Models.Users;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Commands;
+public static class ProposeTopic
+{
+    public record Command(string UserEmail, long TutorId, long FieldOfStudyId, int MaxRealizationNumber, string PolishName, string EnglishName, string Message) : IRequest<Unit>;
+
+    public class Handler : IRequestHandler<Command, Unit>
+    {
+        private readonly DbContext _dbContext;
+
+        public Handler(DbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var user = _dbContext.Set<User>().FirstOrDefault(x => x.Email.Address == request.UserEmail);
+
+            var fieldOfStudy = _dbContext.Set<FieldOfStudy>().FirstOrDefault(x => x.Id == request.FieldOfStudyId);
+
+            var studentFieldOfStudy = _dbContext.Set<StudentFieldOfStudy>().FirstOrDefault(x => x.StudentId == user.Id && x.FieldOfStudyId == fieldOfStudy.Id);
+
+            if (user == null || fieldOfStudy == null || studentFieldOfStudy == null) throw new InvalidOperationException("Application cannot be created with given data");
+
+            var topic = new Topic
+            {
+                Proposer = user,
+                IsProposedByStudent = true,
+                MaxRealizationNumber = request.MaxRealizationNumber,
+                IsAccepted = false,
+                FieldOfStudy = fieldOfStudy,
+                Name = request.PolishName,
+                EnglishName = request.EnglishName,
+                YearOfDefence = studentFieldOfStudy.PlannedYearOfDefence
+            };
+
+            await _dbContext.Set<Topic>().AddAsync(topic).ConfigureAwait(false);
+
+            var application = new Core.Models.Topics.Application
+            {
+                Submitter = user as Student,
+                Topic = topic,
+                Timestamp = DateTime.Now,
+                Message = request.Message,
+                IsTopicProposal = true
+            };
+
+            await _dbContext.Set<Core.Models.Topics.Application>().AddAsync(application).ConfigureAwait(false);
+
+            return Unit.Value;
+        }
+    }
+
+}
--- src/WebApi/Controllers/TopicsController.cs
+++ src/WebApi/Controllers/TopicsController.cs
@@ -1,4 +1,5 @@
-﻿using Application.Queries;
+﻿using Application.Commands;
+using Application.Queries;
 using Application.Queries.Dtos;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -18,11 +19,26 @@ public class TopicsController : BaseApiController
         _mediator = mediator;
     }
 
+    /// <summary>
+    /// Returns all topics grouped by field of study and year of defence
+    /// </summary>
+    /// <returns>All topics grouped by field of study and year of defence</returns>
+    /// <response code="200">Returns all topics grouped by field of study and year of defence</response>
     [HttpGet]
     [Authorize(Roles = Role.StudentRole + "," + Role.TutorRole)]
     public Task<IEnumerable<FieldOfStudyInitialTableDto<StudentsTopicDto>>> GetAllTopicsAsync(CancellationToken cancellationToken)
     {
         return _mediator.Send(new GetAllTopics.Query(), cancellationToken);
+    }
+
+    [HttpPost]
+    [Authorize(Roles = Role.StudentRole)]
+    public Task ProposeTopic([FromQuery] long TutorId, [FromQuery] long FieldOfStudyId,
+        [FromQuery] int MaxRealizationNumber, [FromQuery] string PolishName, [FromQuery] string EnglishName,
+        [FromQuery] string Message, CancellationToken cancellationToken)
+    {
+        string email = GetUserEmail();
+        return _mediator.Send(new ProposeTopic.Command(email, TutorId, FieldOfStudyId, MaxRealizationNumber, PolishName, EnglishName, Message), cancellationToken);
     }
     
 
