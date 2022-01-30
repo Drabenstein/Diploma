c3e1f2b5804f41e58a02b8775b8585e03d497e2d
Dominik Bordzio <dbordzio@gmail.com>
2022-01-30 22:03:25 +0100

    Poprawki 03 - wiecej danych do wyswietlania pracy

--- src/Application/Queries/Dtos/MyThesisDto.cs
+++ src/Application/Queries/Dtos/MyThesisDto.cs
@@ -3,8 +3,15 @@
 public class MyThesisDto
 {
     public long Id { get; set; }
-    public string Topic { get; set; }
+    public string TopicName { get; set; }
+    public string TopicEnglishName { get; set; }
     public string Status { get; set; }
+    public string? Language { get; set; }
+    public bool? HasConsentToChangeLanguage { get; set; }
+    public string SupervisorFullName { get; set; }
+    public string YearOfDefence { get; set; }
+    public string FieldOfStudy { get; set; }
+    
     public IEnumerable<ReviewForMyThesisDto> Reviews { get; set; }
 }
 
--- src/Application/Queries/GetMyThesis.cs
+++ src/Application/Queries/GetMyThesis.cs
@@ -1,14 +1,10 @@
-﻿using Application.Queries.Dtos;
+﻿using Application.Common;
+using Application.Queries.Dtos;
 using Core.Models.Theses;
 using Core.Models.Users;
 using FluentValidation;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Queries;
 public static class GetMyThesis
@@ -28,22 +24,35 @@ public static class GetMyThesis
         {
             var user = await _dbContext.Set<Student>().FirstOrDefaultAsync(x => x.Email == request.Email).ConfigureAwait(false);
 
-            var thesis = await _dbContext.Set<Thesis>().FirstOrDefaultAsync(x => x.Id == request.ThesisId).ConfigureAwait(false);
+            var thesis = await _dbContext.Set<Thesis>()
+                .Include(x => x.Topic)
+                    .ThenInclude(x => x.Supervisor)
+                .Include(x => x.Topic)
+                    .ThenInclude(x => x.FieldOfStudy)
+                .Include(x => x.Reviews)
+                    .ThenInclude(x => x.Reviewer)
+                .FirstOrDefaultAsync(x => x.Id == request.ThesisId)
+                .ConfigureAwait(false);
 
-            if (user == null || thesis == null) throw new InvalidOperationException("Cannot find thesis with given data");
-
+            if (user is null || thesis is null) throw new InvalidOperationException("Cannot find thesis with given data");
             return new MyThesisDto
             {
                 Id = request.ThesisId,
-                Topic = thesis.Topic.Name,
+                TopicName = thesis.Topic.Name,
+                TopicEnglishName = thesis.Topic.EnglishName,
                 Status = thesis.Status.ToString(),
+                Language = thesis.Language?.ToString(),
+                HasConsentToChangeLanguage = thesis.HasConsentToChangeLanguage,
+                SupervisorFullName = ($"{thesis.Topic.Supervisor.FirstName} {thesis.Topic.Supervisor.LastName}".CombineAcademicDegreeAndFullName(thesis.Topic.Supervisor.AcademicDegree.ToString())),
+                YearOfDefence = thesis.Topic.YearOfDefence,
+                FieldOfStudy = thesis.Topic.FieldOfStudy.Name,
                 Reviews = thesis.Reviews
                 .OrderBy(x => x.Reviewer.LastName)
                 .ThenBy(x => x.Reviewer.FirstName)
                 .Select(x => new ReviewForMyThesisDto
                 {
                     Id = x.Id,
-                    Grade = x.Grade == null ? null : x.Grade.ToString(),
+                    Grade = x.Grade?.ToString(),
                     Timestamp = x.PublishTimestamp,
                     Reviewer = $"{x.Reviewer.FirstName} {x.Reviewer.LastName}"
                 })
